c610bb703b31255230e2a68a6be7a0860a25aeea
Nevohteeb <ciaranslow@outlook.com>
2024-06-25 13:35:43 +1200

    updated homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Homepage = () => {
+  const navigate = useNavigate();
+
+  const handleButton = () => {
+    navigate('/search');
+  }
   return (
-    <div>
-      <h1>Find your next NZ Stay</h1>
-      <p>Curated accomodation from all around New Zealand</p>
+    <div className='home-container'>
+      <div className='home-info'>
+        <h1>Find your next NZ Stay</h1>
+        <p>Curated accomodation from all around New Zealand.</p>
+        <div className='button-container'>
+          <button onClick={handleButton}>Search Rooms</button>
+        </div>
+      </div>
     </div>
   )
 }
